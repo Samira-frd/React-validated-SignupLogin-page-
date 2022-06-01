e76bbb32c162ddd8cb1e5e7097623cbbf4cb51fe
Samira Farokhnejad <samira.frd@hotmail.com>
2022-06-01 16:05:18 +1000

    first commit

---
+++ src/components/validate.js
@@ -0,0 +1,43 @@
+export const validate = (data) => {
+  const errors = {};
+
+  if (!data.name.trim()) {
+    errors.name = "Username is required.";
+  } else {
+    delete errors.name;
+  }
+
+  if (!data.email) {
+    errors.email = "Email is required.";
+  } else if (
+    !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email)
+  ) {
+    errors.email = "Please enter a valid email.";
+  } else {
+    delete errors.email;
+  }
+
+  if (!data.password) {
+      errors.password = "Password is required."
+  } else if(data.password.length < 6 ) {
+      errors.password = "Password cannot be less than 6 characters."
+  } else {
+      delete errors.password;
+  }
+
+  if (!data.confirmPassword) {
+      errors.confirmPassword = "Confirm Password is required."
+  } else if(data.confirmPassword !== data.password) {
+      errors.confirmPassword = "Passwords do not match."
+  } else {
+      delete errors.confirmPassword;
+  }
+
+  if (data.isAccepted) {
+      errors.isAccepted = "Please accept our policy."
+  } else {
+      delete errors.isAccepted;
+  }
+
+  return errors;
+};
